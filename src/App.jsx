fd7a06fc05c7fc88470224118c8f605aba3671e7
Benjamin <benjamin.morales3@mail.udp.cl>
2024-03-14 09:58:53 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import {NextUIProvider} from "@nextui-org/react";
+import './App.css';
+
+/*
+import Wave from "react-wavify";
+
+<Wave
+  fill = "#1277b0"
+  paused= {false}
+  options = {
+    {
+      height: 80,
+      amplitude: 40,
+      speed: 0.20,
+      points: 4
+    }
+  }
+/>
+
+*/
+
+function App() {
+  return (
+  <NextUIProvider>
+    <>
+      
+    </>
+  </NextUIProvider>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    addDynamicIconSelectors(),
+    nextui()
+],
+}
+
